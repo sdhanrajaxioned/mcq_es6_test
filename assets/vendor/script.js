7bcea8915f79e8508ddf711d38b033d7bce0eb0b
sdhanrajaxioned <dhanrajs@axioned.com>
2022-06-17 15:21:41 +0530

    added fetch getquestion starttest functionality

---
+++ assets/vendor/script.js
@@ -0,0 +1,77 @@
+const question = document.querySelector('.question'),
+  questionContainer = document.querySelector('.question-container'),
+  choices = Array.from(document.getElementsByClassName('choice-text'));
+
+let currentQuestion = {},
+  acceptingAnswers = false,
+  score = 0,
+  questionCounter = 0,
+  availableQuestions = [],
+  questions = [];
+
+const maxQuestions = 10;
+
+fetch('https://opentdb.com/api.php?amount=10&category=9&difficulty=medium&type=multiple')
+  .then((response) => response.json())
+  .then((loadedQuestions) => {
+    questions = loadedQuestions.results.map((loadedQuestion) => {
+      const formattedQuestion = {
+        question: loadedQuestion.question,
+      };
+      const answerChoices = [...loadedQuestion.incorrect_answers];
+      formattedQuestion.answer = Math.floor(Math.random() * 4) + 1;
+      answerChoices.splice(formattedQuestion.answer-1, 0, loadedQuestion.correct_answer);
+      answerChoices.forEach((choice, index) => formattedQuestion['choice' + (index + 1)] = choice);
+      return formattedQuestion;
+    });
+    startTest();
+  })
+  .catch((error) => alert.error(error));
+  
+  
+const startTest = () => {
+  questionCounter = 0;
+  score = 0;
+  availableQuestions = [...questions];
+  getNewQuestion();
+}
+
+getNewQuestion = () => {
+  if(availableQuestions.length === 0 || questionCounter >= maxQuestions) {
+    localStorage.setItem('score', score);
+      //go to the end page
+      return window.location.assign('end.html');
+  }
+
+  questionCounter++;
+  const questionIndex = Math.floor(Math.random() * availableQuestions.length);
+  currentQuestion = availableQuestions[questionIndex];
+    question.innerText = currentQuestion.question;
+
+    choices.forEach((choice) => {
+        const number = choice.dataset['number'];
+        choice.innerText = currentQuestion['choice' + number];
+    });
+
+    availableQuestions.splice(questionIndex, 1);
+    acceptingAnswers = true;
+};
+
+choices.forEach((choice) => {
+  choice.addEventListener('click', (e) => {
+    if(!acceptingAnswers) return;
+
+    acceptingAnswers = false;
+    const selectedChoice = e.target;
+    const selectedAnswer = selectedChoice.dataset['number'];
+
+    const classToApply = selectedAnswer == currentQuestion.answer ? 'correct' : 'incorrect';
+
+    selectedChoice.parentElement.classList.add(classToApply);
+
+    setTimeout(() => {
+      selectedChoice.parentElement.classList.remove(classToApply);  
+      getNewQuestion();
+    }, 1000);
+  });
+});
